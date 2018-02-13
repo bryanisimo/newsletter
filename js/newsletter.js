ac1803d865ad30029c2506ee5e99a528c438ed03
Bryan Gonzalez <bryanisimo@gmail.com>
2018-02-12 22:12:49 -0600

    Delegate

--- js/newsletter.js
+++ js/newsletter.js
@@ -5,6 +5,22 @@ var datos_contacto = {
 };
 $(document).ready(function(){
   $("#fila-2,#fila-3,#fila-4,#fila-5").hide();
+  $('#formulario').on('submit',function( event ){
+    event.preventDefault();
+  })
+  $('#fila-4').delegate('input','keyup',function(){
+    var todo_bien = true;
+    $('#fila-4').find('input').each(function(){
+      if( $( this ).val() == '' ){
+        todo_bien = false;
+      }
+    });
+    if( todo_bien ){
+      $('#fila-5').show();
+    }else{
+      $('#fila-5').hide();
+    }
+  });
   // Input de correo
   $("input#correo").on('keyup', function(){
     datos_contacto.correo = $("input#correo").val().trim();
@@ -26,9 +42,21 @@ $(document).ready(function(){
   });
 
   // Input de numero
-  $("input#numero").on('keyup', function(){
+  $("input#numero").on('keyup change', function(){
     var numero = parseInt( $("input#numero").val().trim() );
     if( numero > 0 ){
+
+      $('#fila-4').html('');
+      var indice = 0;
+      while( indice < numero ){
+        $('#fila-4').append(
+          '<div class="col">'+
+            '<p>Se llama</p>'+
+            '<input type="text" class="form-control">'+
+          '</div>'
+        );
+        indice++;
+      }
       $('#fila-4').show();
     }else{
       $('#fila-4').hide();
